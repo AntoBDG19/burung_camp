a598c5c5bfedc80f254e8bbebc396051f1631056
notxGETLINK <akhmadyulianto@gmail.com>
2019-01-27 14:24:31 +0700

    end test

---
+++ burung/api/resources/test.py
@@ -0,0 +1,9 @@
+from flask_jwt_extended import jwt_required
+from flask_restful import Resource
+
+
+class TestResource(Resource):
+    method_decorators = [jwt_required]
+
+    def get(self):
+        return {"Test": "tes burung"}
